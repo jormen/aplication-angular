6a9f9906e149849b8c01dabf49a46efe35963707
JorgeMenG <jorgemen1918@gamil.com>
2021-06-05 12:58:23 -0500

    frontend

--- src/app/modules/user/user-detail/user-detail.component.ts
+++ src/app/modules/user/user-detail/user-detail.component.ts
@@ -1,4 +1,5 @@
 import { Component, OnInit } from '@angular/core';
+import { ActivatedRoute } from '@angular/router';
 
 @Component({
   selector: 'app-user-detail',
@@ -7,7 +8,13 @@ import { Component, OnInit } from '@angular/core';
 })
 export class UserDetailComponent implements OnInit {
 
-  constructor() { }
+  public id: number;
+
+  constructor(
+    private route: ActivatedRoute
+  ) {
+    this.id=this.route.snapshot.params.id;
+   }
 
   ngOnInit(): void {
   }
--- src/app/modules/user/user-list/user-list.component.ts
+++ src/app/modules/user/user-list/user-list.component.ts
@@ -10,6 +10,7 @@ export class UserListComponent implements OnInit {
 
 public users: ICardUser[] = [
   {
+    id: 1,
     name: 'Juan Gonzalez ',
     age: 34,
     description: 'Desarrollador fullstack',
@@ -17,36 +18,42 @@ public users: ICardUser[] = [
     work: 'Project manager'
   },
   {
+    id: 2,
     name: 'Jose Lopez ',
     age: 44,
     description: 'Desarrollador backend',
     avatar: 'https://st3.depositphotos.com/1007566/14344/v/1600/depositphotos_143445607-stock-illustration-colorful-silhouette-faceless-half-body.jpg'
   },
   {
+    id: 3,
     name: 'Sandra Reyes ',
     age: 24,
     description: 'Secretaria',
     avatar: 'https://image.freepik.com/foto-gratis/retrato-medio-cuerpo-femenino-hermoso-aislado-fondo-color-rosa-estudio-joven-mujer-sorprendida-emocional_155003-8606.jpg'
   },
   {
+    id: 4,
     name: 'Luis Salazar ',
-    age: 27,
+    age: 57,
     description: 'Desarrollador frontend',
     avatar: 'https://ntrinitarias.com/wp-content/uploads/Capture-28.jpg'
   },
   {
+    id: 5,
     name: 'Alicia Restrepo ',
-    age: 54,
+    age: 34,
     description: 'Analista',
     avatar: 'https://st3.depositphotos.com/7358190/15593/v/1600/depositphotos_155935690-stock-illustration-surprised-attractive-plump-brunette-woman.jpg'
   },
   {
+    id: 6,
     name: 'Carlos Rodriguez ',
     age: 29,
     description: 'Desarrollador backend',
     avatar: 'https://st2.depositphotos.com/1007566/11541/v/450/depositphotos_115418224-stock-illustration-young-businessman-profile-with-an.jpg'
   },
   {
+    id: 7,
     name: 'Sebastian Mendoza ',
     age: 36,
     description: 'Desarrollador backend',
